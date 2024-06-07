11a45298d35c577e25333d1782f0f4e3dcf2d592
Emanuel Orozco <emanuelorozco2011@hotmail.com>
2024-06-07 00:34:25 -0500

    commit pal pueblo numero 5

--- cifradorMensajes/modelo/errores.py
+++ cifradorMensajes/modelo/errores.py
@@ -2,31 +2,35 @@ from abc import ABC, abstractmethod
 
 
 class ErrorContenido(ABC, Exception):
-
     def __init__(self, detalles: str):
-        self.detalles: str = detalles
+        super().__init__(detalles)
 
 
 class ContieneNoAscci(ErrorContenido):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, detalles: str):
+        super().__init__(detalles)
 
 
 class ContieneNumero(ErrorContenido):
+    def __init__(self, detalles: str):
+        super().__init__(detalles)
+
+
+class ErrorFormato(ABC, Exception):
     def __init__(self):
         super().__init__()
 
-class ErrorFormato(ABC, Exception):
-    ...
-
 
 class DobleEspacio(ErrorFormato):
-    ...
+    def __init__(self):
+        super().__init__()
 
 
 class SinLetras(ErrorFormato):
-    ...
+    def __init__(self):
+        super().__init__()
 
 
 class NoTrim(ErrorFormato):
-    ...
+    def __init__(self):
+        super().__init__()
