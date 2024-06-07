f9db850745ff1cccb950d2f56990d84d7d264ef3
Emanuel Orozco <emanuelorozco2011@hotmail.com>
2024-06-06 20:56:59 -0500

    second commit

--- cifradorMensajes/modelo/errores.py
+++ cifradorMensajes/modelo/errores.py
@@ -1,24 +1,31 @@
 from abc import ABC, abstractmethod
+
 class ErrorContenido(ABC, Exception):
     @abstractmethod
     def __init__(self, detalles: str):
         ...
 
+
 class ContieneNoAscci(ErrorContenido):
     def __init__(self):
         super().__init__()
 
+
 class ContieneNumero(ErrorContenido):
-    if ReglaCifrado.encontrar_numeros_mensaje()
+    ...
+
 
 class ErrorFormato(ABC, Exception):
     ...
 
+
 class DobleEspacio(ErrorFormato):
     ...
 
+
 class SinLetras(ErrorFormato):
     ...
+
 
 class NoTrim(ErrorFormato):
     ...
